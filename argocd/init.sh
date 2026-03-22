95475e9eefe0bf12243c2f28bc13e4f5027daf49
rithik-sandron <rithik.sandron@merantix-momentum.com>
2026-03-22 16:01:02 +0100

    updates

--- argocd/init.sh
+++ argocd/init.sh
@@ -1,6 +1,3 @@
 kubectl apply -k base --server-side --force-conflicts
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj-labs/argocd-image-updater/stable/config/install.yaml
 kubectl apply -f sync
-# argocd app sync argocd
-# argocd app sync app-prod
-
