aa3e937491b009bc10d003b08588c7cf51c9d751
rithik-sandron <rithik.sandron@merantix-momentum.com>
2026-03-22 09:25:18 +0100

    updates

--- argocd/init.sh
+++ argocd/init.sh
@@ -1,4 +1,4 @@
-kubectl apply -k base --server-side --force-conflicts
+# kubectl apply -k base --server-side --force-conflicts
 kubectl apply -f sync
 # argocd app sync argocd
 # argocd app sync app-prod
